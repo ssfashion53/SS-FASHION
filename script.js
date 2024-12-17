614a5b5f92ffc10d60593f379ce9a44393d85660
ssfashion53 <ssfashion051@gmail.com>
2024-12-17 21:40:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript code can be used for additional functionalities
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Photographer Template loaded successfully!');
+});
